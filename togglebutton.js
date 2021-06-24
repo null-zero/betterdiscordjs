60bc0685f234852132e267880d16c09ed2322ffc
null-zero <trueshrike7@gmail.com>
2021-06-24 15:31:34 -0500

    Update togglebutton.js

--- togglebutton.js
+++ togglebutton.js
@@ -1,1 +1,8 @@
-
+ function showOrHideChannelTabs() {
+    var v = document.getElementByClass("channelTabs-tabContainer");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ }
