1e915d88e093d11ba2ed488e3fad43a0fa165530
null-zero <trueshrike7@gmail.com>
2022-02-16 06:03:30 -0600

    Delete togglebutton.js

--- togglebutton.js
+++
@@ -1,8 +0,0 @@
- function showOrHideChannelTabs() {
-    var v = document.getElementByClass("channelTabs-tabContainer");
-    if (v.style.display === "none") {
-       v.style.display = "block";
-    } else {
-       v.style.display = "none";
-    }
- }
